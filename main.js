f59cd8b01179a79fff7a9683552adfc1a4f159c9
Andrew Loza <andrejloza81@gmail.com>
2023-02-14 18:03:43 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let a = +prompt("enter number");
+let b = +prompt("enter number");
+let c = a + b;
+
+if (c > 10) {
+  alert("OK");
+} else {
+  alert("false");
+}
+
+let d = prompt("Enter your name");
+
+if (d == "andrew") {
+  alert("hello");
+} else {
+  alert("who are you");
+}
